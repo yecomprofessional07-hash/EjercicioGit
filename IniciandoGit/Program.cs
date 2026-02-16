a14ea5354173b23e25659d278c5b460ef4809617
yecomprofessional07-hash <yecomprofessional07@gmail.com>
2026-02-15 22:39:18 -0600

    Cuarto commit

--- IniciandoGit/Program.cs
+++ IniciandoGit/Program.cs
@@ -1,3 +1,16 @@
 ﻿//Nombre: Erin Yareth Soza Euceda
 
 //Número de cuenta: 20241900227
+
+using System;
+
+// 1. Crear el objeto usando el Constructor
+// Pasamos: usuario, contraseña, nivel y la fecha actual
+comiteado miUsuario = new comiteado("Admin", "1234", 1, DateTime.Now);
+
+// 2. Llamar a los métodos
+miUsuario.MostrarNombre();
+miUsuario.ValidarPassword();
+
+// 3. Acceder a un atributo público
+Console.WriteLine($"Tu nivel actual es: {miUsuario.level}");
---
+++ IniciandoGit/comietado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class comiteado
+{
+    public string user;
+    public string password; // Atributo
+    public int level;
+    DateTime log;
+
+    public comiteado(string user, string password, int level, DateTime log)
+    {
+        this.user = user;
+        this.password = password;
+        this.level = level;
+        this.log = log;
+    }
+
+    // Cambié el nombre a 'ValidarPassword' para que no choque con el atributo 'password'
+    public void ValidarPassword()
+    {
+        Console.WriteLine("INGRESE LA CONTRASEÑA:");
+        string temp = Console.ReadLine() ?? "-/-/-";
+        if (temp != this.password)
+            Console.WriteLine("Error de Autentificación");
+        else
+            Console.WriteLine("Autentificación realizada con éxito");
+    }
+
+    public void MostrarNombre()
+    {
+        Console.WriteLine($"Bienvenido: {user} acabas de ingresar el: {log}");
+    }
+}
+
